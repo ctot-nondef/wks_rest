a7cc23c7d1d0776232c9a1dc45089e8e85976087
Christoph Hoffmann <Christoph.Hoffmann@oeaw.ac.at>
2018-12-10 15:10:06 +0100

    image processing

--- routes/router.js
+++ routes/router.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const USER = require('../lib/auth.js');
 const SCHEMA = require('../lib/schema.js');
 const IMPORT = require('../lib/import.js');
+const ASSETS = require('../lib/asset.js');
 const CONFIG =  require('../config.json');
 
 USER.initUser();
@@ -244,11 +245,14 @@ router.get(`/api/v${CONFIG.version}/jsonschema/:name`, function(req, res, next) 
  *       500:
  *         description: Image processing failed
 */
-router.post(`/api/v${CONFIG.version}/upload`, USER.chkSession , function(req, res, next) {
+router.post(`/api/v${CONFIG.version}/upload`, function(req, res, next) {
+  console.log(req.body);
   if (req.files && req.files.image) {
     let image = req.files.image;
     let name = `${Date.now().valueOf().toString(36)}_${image.name}`
-    image.mv(`./asset/img/${name}`, function(err) {
+    image.mv(`asset/img/${name}`, function(err) {
+      ASSETS.makeImgThumb(`asset/img/${name}`, {width: 220, height: 220}, 90, 'thumb');
+      ASSETS.makeImgThumb(`asset/img/${name}`, {width: 1500, height: 1500}, 90, 'preview');
       if (err) return res.status(500).json({error:'Processing failed'});
       SCHEMA.mongooseModelByName('assetref').create({
         name: image.name,
